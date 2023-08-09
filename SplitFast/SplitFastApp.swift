c19181f904c2244238306b2a12d9589efd45bafd
luka dodelia <luka@lnh.ge>
2023-08-09 13:21:42 +0400

    Initial Commit

---
+++ SplitFast/SplitFastApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitFastApp.swift
+//  SplitFast
+//
+//  Created by l on 09.08.23.
+//
+
+import SwiftUI
+
+@main
+struct SplitFastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
